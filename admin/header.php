1c45e8cb8f18b6b277a1020cc951bf84faa67deb
WeaKaDiKa <weakadika@gmail.com>
2025-05-28 22:20:35 +0800

    modified:   admin/assets/css/mobile-modules.css 	modified:   admin/assets/css/responsive.css 	modified:   admin/header.php

--- admin/header.php
+++ admin/header.php
@@ -20,11 +20,12 @@ try {
 <header class="flex items-center justify-between bg-blue-300 px-6 py-3 border-b border-gray-300">
     <!-- Welcome Message Section -->
     <div class="flex items-center space-x-3 text-gray-900 text-sm font-normal">
-        <span><?php echo $greeting; ?></span>
-        <span class="font-bold text-gray-900 text-base">
+        <span class="lg:block hidden"><?php echo $greeting; ?></span>
+        <span class="font-bold text-gray-900 text-base sm:block hidden">
             <?php echo htmlspecialchars($_SESSION['user_name'] ?? ''); ?>!
         </span>
-        <span class="text-gray-900">
+
+        <span class="text-gray-900 lg:block hidden">
             | North Fairview Branch
         </span>
     </div>
@@ -32,8 +33,8 @@ try {
     <!-- Action Buttons Section -->
     <div class="flex items-center space-x-4">
         <a href="walk_in_appointment.php"
-            class="bg-purple-700 text-white text-xs font-semibold rounded-md px-4 py-1 hover:bg-purple-800">
-            Walk-in Appointment Form
+            class="bg-purple-700 text-white text-xs font-semibold rounded-md hover:bg-purple-800 p-3 text-center">
+            Walk-in Form
         </a>
         <?php include('admin_notification_bell.php'); ?>
         <img alt="Profile photo of <?php echo htmlspecialchars($_SESSION['user_name'] ?? ''); ?>"
