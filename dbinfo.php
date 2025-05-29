635c1bed1b06da50ad63dbdcf6d2cbc1b35ca55c
WeaKaDiKa <weakadika@gmail.com>
2025-05-29 21:19:15 +0800

    modified:   dbinfo.php

--- dbinfo.php
+++ dbinfo.php
@@ -1,10 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "dental_clinic";
+$servername = "sql104.infinityfree.com";
+$username = "if0_36164163"; 
+$password = "Zh2M744E4KwckU"; 
+$dbname = "if0_36164163_dental_clinic";
+
+//$servername = "localhost";
+//$username = "root"; 
+//$password = ""; 
+//$dbname = "dental_clinic";
 
 
 $port = 3306;
 
 $domain = "https://theceprog.great-site.net";
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
