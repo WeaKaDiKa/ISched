0b92a52c0f4615e2d17d426db074325950459d6c
WeaKaDiKa <96182978+WeaKaDiKa@users.noreply.github.com>
2025-06-19 08:25:22 +0800

    Update dbinfo.php

--- dbinfo.php
+++ dbinfo.php
@@ -1,26 +1,10 @@
 <?php
-
-$servername = "sql213.infinityfree.com";
-$username = "if0_38975384"; 
-$password = "2qHptzN96wi4"; 
-$dbname = "if0_38975384_dental_clinic";
-
-
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "dental_clinic";
-
 $servername = "127.0.0.1";
 $username = "u375599221_roottt"; 
 $password = "!RC2&C>u[FqgkR=2"; 
 $dbname = "u375599221_dental_clinic";
 
-
-$port = 3318;
 $port = 3306;
-
-$domain = "https://.net";
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
