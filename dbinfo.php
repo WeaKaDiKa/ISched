3c746c938a5105d03a1a911a4a0e2200fe6e7153
WeaKaDiKa <weakadika@gmail.com>
2025-08-17 13:21:23 +0800

    modified:   dbinfo.php

--- dbinfo.php
+++ dbinfo.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "127.0.0.1";
-$username = "u375599221_roott"; 
-$password = "!RC2&C>u[FqgkR=2"; 
-$dbname = "u375599221_dental_clinic";
+$servername = "sql104.infinityfree.com";
+$username = "if0_36164163"; 
+$password = "Zh2M744E4KwckU"; 
+$dbname = "if0_36164163_dental_clinic";
 
 $port = 3306;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
